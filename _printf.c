d6c916fa1a728a32a81909b19b23dff067f34463
Collins Wamucii <collinswamucii@gmail.com>
2023-03-28 05:32:10 -0700

    Fix again

--- _printf.c
+++ _printf.c
@@ -1,48 +1,55 @@
-#include "main.h"
-
+#include <stdio.h>
+#include "_putchar.c"
+#include <stdarg.h>
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
- * Authors: Ehoneah Obed & Abdulhakeem Badejo
+ * _printf - prints format to stdout
+ * @format: format to be printed
+ * Return: chars printed
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list args;
 
-	register int count = 0;
+	int chars_printed = 0, pos = 0;
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	va_start(args, format);
+
+	while (format[pos] != '\0')
 	{
-		if (*p == '%')
+		if (format[pos] == '%')
 		{
-			p++;
-			if (*p == '%')
+			if (format[pos + 1] == 'c')
 			{
-				count += _putchar('%');
-				continue;
+				int ch = va_arg(args, int);
+
+				_putchar(ch);
+				chars_printed++;
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			else if (format[pos + 1] == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				int i = 0;
+
+				while (str[i] != '\0')
+				{
+					_putchar(str[i]);
+					i++;
+					chars_printed++;
+				}
+			}
+			else if (format[pos + 1] == '%')
+			{
+				_putchar('%');
+				chars_printed++;
+			}
+		}
+		else
+		{
+			_putchar(format[pos]);
+			chars_printed++;
+		}
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+		va_end(args);
+		return (chars_printed);
 }
